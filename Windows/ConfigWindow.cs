6dd4830a8ba666eb94c20b1a3826f6fd715df2c7
KuroZantetsuken <22131454+KuroZantetsuken@users.noreply.github.com>
2026-05-11 00:49:29 +0200

    refactor(ui): redesign configuration window layout and add usage guide

--- Windows/ConfigWindow.cs
+++ Windows/ConfigWindow.cs
@@ -14,36 +14,35 @@ namespace Slackingway.Windows
         private readonly Configuration configuration;
         private readonly Plugin plugin;
 
+        private readonly Vector2 windowSize = new Vector2(260, 320);
+
         private CheckboxNode enableLimiterNode = null!;
         private CheckboxNode enableLoggingNode = null!;
         private TextNode targetGpuUsageLabelNode = null!;
         private NumericInputNode targetGpuUsageNode = null!;
         private TextNode lowerTextNode = null!;
-        private TextNode recommendedTextNode = null!;
         private HorizontalLineNode separatorNode = null!;
-        private TextButtonNode calibrateButtonNode = null!;
-        private TextNode calibrationStatusNode = null!;
-        private TextNode calibrateDescNode = null!;
+        private HorizontalLineNode separatorNode2 = null!;
+        private TextNode guideHeaderNode = null!;
+        private TextNode guideTextNode = null!;
         private TextNode currentGpuUsageNode = null!;
-        private TextNode baselineMaxUsageNode = null!;
 
         public ConfigWindow(Plugin plugin)
         {
             this.configuration = plugin.Configuration;
             this.plugin = plugin;
-            this.Size = new Vector2(450, 360);
+            this.Size = windowSize;
         }
 
         protected override unsafe void OnSetup(AtkUnitBase* addon, Span<AtkValue> atkValueSpan)
         {
-            var contentStart = ContentStartPosition + new Vector2(10, 10);
-            var innerWidth = Size.X - 20f - ContentStartPosition.X;
+            var innerWidth = windowSize.X - 10f - ContentStartPosition.X;
 
             enableLimiterNode = new CheckboxNode
             {
                 String = "Enable Limiter",
                 IsChecked = configuration.IsEnabled,
-                Position = contentStart,
+                Position = ContentStartPosition,
                 Size = new Vector2(150, 24),
                 OnClick = val =>
                 {
@@ -55,9 +54,9 @@ namespace Slackingway.Windows
 
             enableLoggingNode = new CheckboxNode
             {
-                String = "Enable Verbose Logging",
+                String = "Enable Logging",
                 IsChecked = configuration.EnableLogging,
-                Position = contentStart + new Vector2(160, 0),
+                Position = ContentStartPosition + new Vector2(0, 20),
                 Size = new Vector2(200, 24),
                 OnClick = val =>
                 {
@@ -67,10 +66,26 @@ namespace Slackingway.Windows
             };
             enableLoggingNode.AttachNode(this);
 
+            separatorNode2 = new HorizontalLineNode
+            {
+                Position = ContentStartPosition + new Vector2(0, 50),
+                Size = new Vector2(innerWidth, 2)
+            };
+            separatorNode2.AttachNode(this);
+
+            currentGpuUsageNode = new TextNode
+            {
+                Position = ContentStartPosition + new Vector2(0, 60),
+                Size = new Vector2(innerWidth, 20),
+                AlignmentType = AlignmentType.TopLeft,
+                FontSize = 14,
+            };
+            currentGpuUsageNode.AttachNode(this);
+
             targetGpuUsageLabelNode = new TextNode
             {
-                String = "Target GPU Usage %",
-                Position = contentStart + new Vector2(0, 40),
+                String = "Target GPU Usage:",
+                Position = ContentStartPosition + new Vector2(0, 80),
                 Size = new Vector2(200, 20),
                 AlignmentType = AlignmentType.TopLeft,
                 FontSize = 14,
@@ -83,7 +98,7 @@ namespace Slackingway.Windows
                 Max = 100,
                 Step = 1,
                 Value = (int)configuration.TargetGpuUsage,
-                Position = contentStart + new Vector2(innerWidth - 100, 35),
+                Position = ContentStartPosition + new Vector2(innerWidth - 100, 75),
                 Size = new Vector2(100, 28),
                 OnValueUpdate = val =>
                 {
@@ -95,111 +110,47 @@ namespace Slackingway.Windows
 
             lowerTextNode = new TextNode
             {
-                String = "Lower until other processes have enough headroom to function properly.",
-                Position = contentStart + new Vector2(0, 75),
+                String = "Lower until other apps have enough headroom to function properly.",
+                Position = ContentStartPosition + new Vector2(0, 110),
                 Size = new Vector2(innerWidth, 40),
                 AlignmentType = AlignmentType.TopLeft,
-                FontSize = 14,
-                LineSpacing = 20,
+                LineSpacing = 16,
                 TextFlags = TextFlags.WordWrap | TextFlags.MultiLine,
             };
             lowerTextNode.AttachNode(this);
 
-            recommendedTextNode = new TextNode
-            {
-                String = "Recommended to set this in a GPU limited scene while playing a video on a secondary monitor.",
-                Position = contentStart + new Vector2(0, 115),
-                Size = new Vector2(innerWidth, 40),
-                AlignmentType = AlignmentType.TopLeft,
-                FontSize = 14,
-                LineSpacing = 20,
-                TextFlags = TextFlags.WordWrap | TextFlags.MultiLine,
-            };
-            recommendedTextNode.AttachNode(this);
-
             separatorNode = new HorizontalLineNode
             {
-                Position = contentStart + new Vector2(0, 165),
+                Position = ContentStartPosition + new Vector2(0, 150),
                 Size = new Vector2(innerWidth, 2)
             };
             separatorNode.AttachNode(this);
 
-            calibrateButtonNode = new TextButtonNode
+            guideHeaderNode = new TextNode
             {
-                String = "Calibrate Baseline",
-                Position = contentStart + new Vector2(0, 175),
-                Size = new Vector2(150, 28),
-                OnClick = () => plugin.StartCalibration()
-            };
-            calibrateButtonNode.AttachNode(this);
-
-            calibrationStatusNode = new TextNode
-            {
-                Position = contentStart + new Vector2(160, 177),
-                Size = new Vector2(200, 24),
-                AlignmentType = AlignmentType.Left,
-                FontSize = 14,
-                IsVisible = false
-            };
-            calibrationStatusNode.AttachNode(this);
-
-            calibrateDescNode = new TextNode
-            {
-                String = "Set this in a GPU limited scene to calibrate peak usage.",
-                Position = contentStart + new Vector2(0, 210),
-                Size = new Vector2(innerWidth, 40),
+                String = "Recommended setup:",
+                Position = ContentStartPosition + new Vector2(0, 160),
+                Size = new Vector2(innerWidth, 20),
                 AlignmentType = AlignmentType.TopLeft,
                 FontSize = 14,
-                LineSpacing = 20,
+            };
+            guideHeaderNode.AttachNode(this);
+
+            guideTextNode = new TextNode
+            {
+                String = "1. Stand in an inn.\n2. Play YouTube with FFXIV in focus.\n3. Lower the 'Target GPU Usage' until the video stops stuttering.\n",
+                Position = ContentStartPosition + new Vector2(0, 185),
+                Size = new Vector2(innerWidth, windowSize.Y - 230),
+                AlignmentType = AlignmentType.TopLeft,
+                LineSpacing = 16,
                 TextFlags = TextFlags.WordWrap | TextFlags.MultiLine,
             };
-            calibrateDescNode.AttachNode(this);
-
-            currentGpuUsageNode = new TextNode
-            {
-                Position = contentStart + new Vector2(0, 250),
-                Size = new Vector2(innerWidth, 20),
-                AlignmentType = AlignmentType.TopLeft,
-                FontSize = 14,
-            };
-            currentGpuUsageNode.AttachNode(this);
-
-            baselineMaxUsageNode = new TextNode
-            {
-                Position = contentStart + new Vector2(0, 270),
-                Size = new Vector2(innerWidth, 20),
-                AlignmentType = AlignmentType.TopLeft,
-                FontSize = 14,
-            };
-            baselineMaxUsageNode.AttachNode(this);
+            guideTextNode.AttachNode(this);
         }
 
         protected override unsafe void OnUpdate(AtkUnitBase* addon)
         {
-            calibrateButtonNode.IsEnabled = !plugin.IsCalibrating;
-
-            if (plugin.IsCalibrating)
-            {
-                calibrationStatusNode.String = "Calibrating...";
-                calibrationStatusNode.TextColor = new Vector4(1, 1, 0, 1);
-                calibrationStatusNode.IsVisible = true;
-            }
-            else
-            {
-                if (plugin.ShowCalibrationSuccess)
-                {
-                    calibrationStatusNode.String = "Calibration Complete!";
-                    calibrationStatusNode.TextColor = new Vector4(0, 1, 0, 1);
-                    calibrationStatusNode.IsVisible = true;
-                }
-                else
-                {
-                    calibrationStatusNode.IsVisible = false;
-                }
-            }
-
             currentGpuUsageNode.String = $"Current GPU Usage: {plugin.LastGpuUsage:F1}%";
-            baselineMaxUsageNode.String = $"Baseline Max Usage: {configuration.BaselineGpuUsage:F0}%";
         }
     }
 }
